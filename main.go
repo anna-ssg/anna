a93ffa069eb59357c438691851d1a009ae0a8360
DedLad <adheshathrey2004@gmail.com>
2024-02-26 13:04:08 +0530

    Migrated to cobra and added yaml rewrite for baseURL

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
 
 	"github.com/acmpesuecc/ssg/cmd/ssg"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	serve := flag.Bool("serve", false, "serve the rendered content")
-	addr := flag.String("addr", ":8000", "ip address to serve rendered content to")
-	flag.Parse()
+	var serve bool
+	var addr string
 
-	generator := ssg.Generator{
-		ErrorLogger: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+	rootCmd := &cobra.Command{
+		Use:   "ssg",
+		Short: "Static Site Generator",
+		Run: func(cmd *cobra.Command, args []string) {
+			generator := ssg.Generator{
+				ErrorLogger: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+			}
+			generator.RenderSite()
+
+			if serve {
+				generator.ServeSite(addr)
+			}
+		},
 	}
-	generator.RenderSite()
 
-	if *serve {
-		generator.ServeSite(*addr)
+	rootCmd.Flags().BoolVarP(&serve, "serve", "s", false, "serve the rendered content")
+	rootCmd.Flags().StringVarP(&addr, "addr", "a", "8000", "ip address to serve rendered content to")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
 	}
 }
