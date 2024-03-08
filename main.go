f1962cfc93f9130944f4c5cb87ac4b05ed93c647
DedLad <adheshathrey2004.com>
2024-03-08 12:22:21 +0530

    profiling added (--prof)

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"html/template"
 	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
+	"time"
 
 	"github.com/acmpesuecc/anna/cmd/ssg"
 	"github.com/spf13/cobra"
@@ -14,16 +17,20 @@ func main() {
 	var addr string
 	var draft bool
 	var validateHTML bool
-
+	var prof bool
+	StartProfiling()
+	startTime := time.Now()
 	rootCmd := &cobra.Command{
 		Use:   "anna",
 		Short: "Static Site Generator",
 		Run: func(cmd *cobra.Command, args []string) {
+
 			generator := ssg.Generator{
 				ErrorLogger: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 				Templates:   make(map[template.URL]ssg.TemplateData),
 				TagsMap:     make(map[string][]ssg.TemplateData),
 			}
+
 			if draft {
 				generator.RenderDrafts = true
 			}
@@ -31,11 +38,26 @@ func main() {
 			if serve {
 				generator.StartLiveReload(addr)
 			}
-			generator.RenderSite(addr)
+
+			if !prof {
+				generator.RenderSite("")
+			}
 
 			if validateHTML {
 				ssg.ValidateHTMLContent()
 			}
+			if prof {
+
+				generator.RenderSite("")
+
+				elapsedTimesince := time.Since(startTime) //this didn't work for some reason and was giving negitive deviation
+				// elapsedTime := time.Now().Sub(startTime)
+
+				PrintStats(elapsedTimesince)
+				// PrintStats(elapsedTime)
+				defer StopProfiling()
+			}
+
 		},
 	}
 
@@ -43,8 +65,58 @@ func main() {
 	rootCmd.Flags().StringVarP(&addr, "addr", "a", "8000", "ip address to serve rendered content to")
 	rootCmd.Flags().BoolVarP(&draft, "draft", "d", false, "renders draft posts")
 	rootCmd.Flags().BoolVarP(&validateHTML, "validate-html", "v", false, "validate semantic HTML")
+	rootCmd.Flags().BoolVar(&prof, "prof", false, "enable profiling")
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func StartProfiling() {
+	runtime.MemProfileRate = 1
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+			memProfile := pprof.Lookup("heap")
+			if memProfile != nil {
+				file, err := os.Create("mem_profile.pprof")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer file.Close()
+				memProfile.WriteTo(file, 1)
+			}
+			cpuProfile := pprof.Lookup("goroutine")
+			if cpuProfile != nil {
+				file, err := os.Create("cpu_profile.pprof")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer file.Close()
+				cpuProfile.WriteTo(file, 1)
+			}
+		}
+	}()
+}
+
+func StopProfiling() {
+	runtime.GC()
+}
+
+func PrintStats(elapsedTime time.Duration) {
+	cpuUsage := runtime.NumCPU()
+	threads := runtime.GOMAXPROCS(0)
+	memStats := new(runtime.MemStats)
+	runtime.ReadMemStats(memStats)
+
+	log.Printf("Threads: %d", threads)
+	log.Printf("Cores: %d", cpuUsage)
+	log.Printf("Time Taken: %s", elapsedTime)
+	log.Printf("Allocated Memory: %d bytes", memStats.Alloc)
+	log.Printf("Total Memory Allocated: %d bytes", memStats.TotalAlloc)
+	log.Printf("Heap Memory In Use: %d bytes", memStats.HeapInuse)
+	log.Printf("Heap Memory Idle: %d bytes", memStats.HeapIdle)
+	log.Printf("Heap Memory Released: %d bytes", memStats.HeapReleased)
+	log.Printf("Number of Goroutines: %d", runtime.NumGoroutine())
 }
