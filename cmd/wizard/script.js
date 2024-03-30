868819ff07d9f8e7b9efde8d0ea58cda4d82a667
Nathan Paul <mail@polarhive.net>
2024-03-30 10:04:12 +0530

    feat/wiz: confetti!!

--- cmd/wizard/script.js
+++ cmd/wizard/script.js
@@ -28,7 +28,7 @@ function checkFormValidity() {
 
     var nameRegex = /^[a-zA-Z0-9\s]+$/;
     var urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*$/;
-    
+
     var authorButton = document.getElementById("authorButton");
     var siteTitleButton = document.getElementById("siteTitleButton");
     var baseURLButton = document.getElementById("baseURLButton");
@@ -59,6 +59,160 @@ function submitForm() {
         body: formData
     });
 
+    tsParticles.load({
+        id: "tsparticles",
+        options: {
+            "fullScreen": {
+                "zIndex": 1
+            },
+            "emitters": [
+                {
+                    "position": {
+                        "x": 0,
+                        "y": 30
+                    },
+                    "rate": {
+                        "quantity": 5,
+                        "delay": 0.15
+                    },
+                    "particles": {
+                        "move": {
+                            "direction": "top-right",
+                            "outModes": {
+                                "top": "none",
+                                "left": "none",
+                                "default": "destroy"
+                            }
+                        }
+                    }
+                },
+                {
+                    "position": {
+                        "x": 100,
+                        "y": 30
+                    },
+                    "rate": {
+                        "quantity": 5,
+                        "delay": 0.15
+                    },
+                    "particles": {
+                        "move": {
+                            "direction": "top-left",
+                            "outModes": {
+                                "top": "none",
+                                "right": "none",
+                                "default": "destroy"
+                            }
+                        }
+                    }
+                }
+            ],
+            "particles": {
+                "color": {
+                    "value": [
+                        "#ffffff",
+                        "#FF0000"
+                    ]
+                },
+                "move": {
+                    "decay": 0.05,
+                    "direction": "top",
+                    "enable": true,
+                    "gravity": {
+                        "enable": true
+                    },
+                    "outModes": {
+                        "top": "none",
+                        "default": "destroy"
+                    },
+                    "speed": {
+                        "min": 10,
+                        "max": 50
+                    }
+                },
+                "number": {
+                    "value": 0
+                },
+                "opacity": {
+                    "value": 1
+                },
+                "rotate": {
+                    "value": {
+                        "min": 0,
+                        "max": 360
+                    },
+                    "direction": "random",
+                    "animation": {
+                        "enable": true,
+                        "speed": 30
+                    }
+                },
+                "tilt": {
+                    "direction": "random",
+                    "enable": true,
+                    "value": {
+                        "min": 0,
+                        "max": 360
+                    },
+                    "animation": {
+                        "enable": true,
+                        "speed": 30
+                    }
+                },
+                "size": {
+                    "value": {
+                        "min": 0,
+                        "max": 2
+                    },
+                    "animation": {
+                        "enable": true,
+                        "startValue": "min",
+                        "count": 1,
+                        "speed": 16,
+                        "sync": true
+                    }
+                },
+                "roll": {
+                    "darken": {
+                        "enable": true,
+                        "value": 25
+                    },
+                    "enable": true,
+                    "speed": {
+                        "min": 5,
+                        "max": 15
+                    }
+                },
+                "wobble": {
+                    "distance": 30,
+                    "enable": true,
+                    "speed": {
+                        "min": -7,
+                        "max": 7
+                    }
+                },
+                "shape": {
+                    "type": "emoji",
+                    "options": {
+                        "emoji": {
+                            "particles": {
+                                "size": {
+                                    "value": 8
+                                }
+                            },
+                            "value": [
+                                "🍚"
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+
+    tsParticles.load("confetti", confettiSettings);
+
     setTimeout(() => {
         window.location.href = 'http://localhost:8000';
     }, 3000);
@@ -71,3 +225,27 @@ document.getElementById("author").addEventListener("input", checkFormValidity);
 document.getElementById("siteTitle").addEventListener("input", checkFormValidity);
 document.getElementById("baseURL").addEventListener("input", checkFormValidity);
 document.getElementById("themeURL").addEventListener("input", checkFormValidity);
+
+// Confetti animation
+const confettiSettings = {
+    particles: {
+        number: {
+            value: 50
+        },
+        size: {
+            value: 3
+        },
+        shape: {
+            type: "circle"
+        },
+        move: {
+            speed: 6
+        },
+        color: {
+            value: "#00FFFF"
+        },
+        opacity: {
+            value: 0.8
+        }
+    }
+};
