3605a39d2bd2e6f799c420d2dac827d48976bf24
DedLad <adheshathrey2004.com>
2024-04-03 20:43:02 +0530

    Changed implementation for theme picker

--- cmd/anna/wizard.go
+++ cmd/anna/wizard.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"gopkg.in/yaml.v3"
@@ -86,3 +87,46 @@ func writeConfigToFile(config Config) error {
 }
 
 var FormSubmittedCh = make(chan struct{})
+
+func handleInputValues(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	themeURL := r.Form.Get("themeURL")
+
+	switch themeURL {
+	case "/static/styles/sudhir.css":
+		executeCurlCommand("curl https://sudhir.live/static/style.css https://cdn.jsdelivr.net/npm/water.css@2/out/water.css --output-dir site/static -o style.css")
+	case "/static/styles/hegde.css":
+		executeCurlCommand("curl https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/tomorrow-night.css https://hegde.live/static/style.css --output-dir site/static -o style.css")
+	case "/static/styles/nathan.css":
+		executeCurlCommand("curl https://polarhive.net/assets/main.css https://polarhive.net/assets/style.css --output-dir site/static -o style.css")
+	case "/static/styles/new.css":
+		executeCurlCommand("curl https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css -o style.css--output-dir site/static")
+	case "/static/styles/pico.css":
+		executeCurlCommand("curl https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/styles/tokyo-night-dark.min.css https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css --output-dir site/static -o style.css")
+	case "/static/styles/simple.css":
+		executeCurlCommand("curl https://cdn.simplecss.org/simple.css --output-dir site/static -o style.css")
+	default:
+		// Handle default case
+		executeCurlCommand("curl default.css")
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func executeCurlCommand(curlCommand string) {
+	cmd := exec.Command(curlCommand)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("Failed to execute curl command: %v", err)
+	}
+}
