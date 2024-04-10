60010583bcb2a72f830d366f31f3fe3182b8fd72
Nathan Paul <mail@polarhive.net>
2024-04-10 14:51:18 +0530

    meta/gh: add bench.yml

--- test/bench.sh
+++ test/bench.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-set -e
-clear
-
 # parameters
 files=1000
 warm=10
 
 # cleanup
 cleanup() {
-    echo "Cleaning up"
+    echo "cleaning up"
     rm -rf /tmp/bench
 }
 trap cleanup EXIT
-
-# echo "bash: cleanup"
-cd /tmp || exit
-rm -rf bench
-mkdir bench
-cd bench
 
 # check if hyperfine is installed
 if ! command -v hyperfine &>/dev/null; then
@@ -26,40 +17,43 @@ if ! command -v hyperfine &>/dev/null; then
 fi
 
 # cloning candidates
-echo "Cloning SSGs"
-git clone https://github.com/acmpesuecc/anna
-git clone https://github.com/anirudhRowjee/saaru
-sleep 1; clear
+echo "clone SSGs"
+git clone https://github.com/acmpesuecc/anna /tmp/bench/anna
+git clone https://github.com/anirudhRowjee/saaru /tmp/bench/saaru
+sleep 1
 
-# benchmark file
-cp anna/site/content/posts/bench.md test.md 
+# copy benchmark file
+cp /tmp/bench/anna/site/content/posts/bench.md /tmp/bench/test.md 
 
-# checkout at v1 and commit
-cd anna && git checkout v1.0.0-alpha; cd ..
-cd saaru && git checkout c17930724fcaad67e1cfa3cc969667d043c1b826; cd ..
-sleep 1; clear
+# checkout specific versions
+cd /tmp/bench/anna && git checkout v1.0.0-alpha && cd ..
+cd saaru && git checkout c17930724fcaad67e1cfa3cc969667d043c1b826 && cd ..
+sleep 1
 
 # build SSGs
-cd anna; go build; cd ..
-cd saaru; cargo build --release; mv ./target/release/saaru .; cd ..
-sleep 1; clear
+echo "build SSGs"
+cd /tmp/bench/anna && go build && cd ..
+cd saaru && cargo build --release && mv ./target/release/saaru . && cd ..
+echo "finished building"
 
-# clean content dirs (no md files other than test.md)
+# clean content/* dirs
 echo "Cleaning content directories"
-rm -rf anna/site/content/posts/*
-rm -rf saaru/docs/src/*
+rm -rf /tmp/bench/anna/site/content/posts/*
+rm -rf /tmp/bench/saaru/docs/src/*
 
 # create multiple copies of the test file
 echo "Spawning $files different markdown files..."
 for ((i = 0; i < files; i++)); do
-    cp test.md "anna/site/content/posts/test_$i.md"
-    cp test.md "saaru/docs/src/test_$i.md"
+    cp /tmp/bench/test.md "/tmp/bench/anna/site/content/posts/test_$i.md"
+    cp /tmp/bench/test.md "/tmp/bench/saaru/docs/src/test_$i.md"
 done
-sleep 1; clear
+sleep 1
 
-# run the benchmark
-echo "Running benchmark with $files md files and $warm warmup runs"
-cd anna; hyperfine -p 'sync' -w $warm "./anna"; cd ..
-cd saaru; hyperfine -p 'sync' -w $warm "./saaru --base-path ./docs"; cd ..
+# cooldown
+echo "cooldown for 30s"
+sleep 30
 
-echo "Benchmarking finished."
+# run hyperfine
+echo "running benchmark: $files md files and $warm warmup runs"
+cd /tmp/bench/anna && hyperfine -p 'sync' -w $warm "./anna" && cd ..
+cd saaru && hyperfine -p 'sync' -w $warm "./saaru --base-path ./docs" && cd ..
