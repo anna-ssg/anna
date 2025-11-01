a50758f9c738dcb3d1cf82b5874e65fa9f11d40b
DedLad <adheshathrey2004@gmail.com>
2025-11-01 20:21:34 +0530

    refactor : Parallel rendering with bounded worker pool

--- pkg/engine/user_engine.go
+++ pkg/engine/user_engine.go
@@ -15,31 +15,62 @@ func (e *Engine) RenderUserDefinedPages(fileOutPath string, templates *template.
 		concurrency = numTemplates
 	}
 
+	if concurrency < 1 {
+		concurrency = 1
+	}
+	// templateURLs := make([]string, 0, numTemplates)
+	// for templateURL := range e.DeepDataMerge.Templates {
+	// 	templateURLs = append(templateURLs, string(templateURL))
+	// }
+
 	templateURLs := make([]string, 0, numTemplates)
 	for templateURL := range e.DeepDataMerge.Templates {
-		templateURLs = append(templateURLs, string(templateURL))
-	}
-
-	var wg sync.WaitGroup
-	semaphore := make(chan struct{}, concurrency)
-
-	for _, templateURL := range templateURLs {
-		if templateURL == ".html" {
+		s := string(templateURL)
+		if s == ".html" {
 			continue
 		}
-
-		wg.Add(1)
-		semaphore <- struct{}{}
-
-		go func(templateURL string) {
-			defer func() {
-				<-semaphore
-				wg.Done()
-			}()
-
-			e.RenderPage(fileOutPath, template.URL(templateURL), templates, e.DeepDataMerge.Templates[template.URL(templateURL)].Frontmatter.Layout)
-		}(templateURL)
+		templateURLs = append(templateURLs, s)
 	}
+
+	// var wg sync.WaitGroup
+	// semaphore := make(chan struct{}, concurrency)
+
+	tasks := make(chan string, concurrency)
+	var wg sync.WaitGroup
+
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for templateURL := range tasks {
+				URL := template.URL(templateURL)
+				layout := e.DeepDataMerge.Templates[URL].Frontmatter.Layout
+				e.RenderPage(fileOutPath, URL, templates, layout)
+			}
+		}()
+	}
+	for _, v := range templateURLs {
+		tasks <- v
+	}
+	close(tasks)
+
+	// for _, templateURL := range templateURLs {
+	// 	if templateURL == ".html" {
+	// 		continue
+	// 	}
+
+	// 	wg.Add(1)
+	// 	semaphore <- struct{}{}
+
+	// 	go func(templateURL string) {
+	// 		defer func() {
+	// 			<-semaphore
+	// 			wg.Done()
+	// 		}()
+
+	// 		e.RenderPage(fileOutPath, template.URL(templateURL), templates, e.DeepDataMerge.Templates[template.URL(templateURL)].Frontmatter.Layout)
+	// 	}(templateURL)
+	// }
 
 	wg.Wait()
 }
