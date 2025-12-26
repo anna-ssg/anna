2763c409ff4e13f554ae9be0db0d7862044d83f9
Nathan Paul <mail@polarhive.net>
2025-12-26 12:22:09 +0530

    ci: caches?

--- test/bench.sh
+++ test/bench.sh
@@ -3,10 +3,12 @@
 files=1000
 warm=10
 
+BASE_DIR=$(pwd)
+
 # cleanup
 cleanup() {
     echo "cleaning up"
-    rm -rf /tmp/bench
+    rm -rf $BASE_DIR/tmp/bench
 }
 trap cleanup EXIT
 
@@ -25,26 +27,42 @@ fi
 echo ""
 echo "clone SSGs"
 echo ""
-git clone --depth=1 https://github.com/anna-ssg/anna /tmp/bench/anna
-git clone --depth=1 https://github.com/anirudhRowjee/saaru /tmp/bench/saaru
-git clone --depth=1 https://github.com/NavinShrinivas/sapling /tmp/bench/sapling
+
+clone_or_pull() {
+  local repo=$1
+  local dir=$2
+  if [ ! -d "$dir" ]; then
+    git clone --depth=1 "$repo" "$dir"
+  else
+    cd "$dir" && git pull --depth=1 --ff-only
+    if [ $? -ne 0 ]; then
+      cd "$(dirname "$dir")"
+      rm -rf "$dir"
+      git clone --depth=1 "$repo" "$dir"
+    fi
+  fi
+}
+
+clone_or_pull https://github.com/anna-ssg/anna $BASE_DIR/tmp/bench/anna
+clone_or_pull https://github.com/anirudhRowjee/saaru $BASE_DIR/tmp/bench/saaru
+clone_or_pull https://github.com/NavinShrinivas/sapling $BASE_DIR/tmp/bench/sapling
 
 # copy benchmark file
-cp /tmp/bench/anna/site/content/posts/bench.md /tmp/bench/test.md
+cp $BASE_DIR/tmp/bench/anna/site/content/posts/bench.md $BASE_DIR/tmp/bench/test.md
 
 echo ""
 echo "build SSGs"
 echo ""
-cd /tmp/bench/anna && go build && cd /tmp/bench
-cd /tmp/bench/anna && GOEXPERIMENT=greenteagc go build -o anna_greentea && cd /tmp/bench
+cd $BASE_DIR/tmp/bench/anna && go build && cd ../..
+cd $BASE_DIR/tmp/bench/anna && GOEXPERIMENT=greenteagc go build -o anna_greentea && cd ../..
 
 # build rust based SSGs (edit this block if they are already installed)
-cd /tmp/bench/sapling && cargo build --release && mv target/release/sapling .
-cd /tmp/bench/saaru && cargo build --release && mv target/release/saaru .
+cd $BASE_DIR/tmp/bench/sapling && cargo build --release && mv target/release/sapling .
+cd $BASE_DIR/tmp/bench/saaru && cargo build --release && mv target/release/saaru .
 
 ## setup hugo
-hugo new site /tmp/bench/hugo; cd /tmp/bench/hugo
-hugo new theme mytheme; echo "theme = 'mytheme'" >> hugo.toml; cd /tmp/bench
+hugo new site $BASE_DIR/tmp/bench/hugo; cd $BASE_DIR/tmp/bench/hugo
+hugo new theme mytheme; echo "theme = 'mytheme'" >> hugo.toml; cd ../..
 
 ## setup 11ty
 
@@ -52,19 +70,19 @@ hugo new theme mytheme; echo "theme = 'mytheme'" >> hugo.toml; cd /tmp/bench
 echo ""
 echo "Cleaning content directories"
 echo ""
-rm -rf /tmp/bench/anna/site/content/posts/*
-rm -rf /tmp/bench/saaru/docs/src/*
-rm -rf /tmp/bench/sapling/benchmark/content/blog/*
-rm -rf /tmp/bench/hugo/content/*
+rm -rf $BASE_DIR/tmp/bench/anna/site/content/posts/*
+rm -rf $BASE_DIR/tmp/bench/saaru/docs/src/*
+rm -rf $BASE_DIR/tmp/bench/sapling/benchmark/content/blog/*
+rm -rf $BASE_DIR/tmp/bench/hugo/content/*
 
 # create multiple copies of the test file
 echo ""
 echo "Spawning $files different markdown files..."
 for ((i = 0; i < files; i++)); do
-    cp /tmp/bench/test.md "/tmp/bench/anna/site/content/posts/test_$i.md"
-    cp /tmp/bench/test.md "/tmp/bench/saaru/docs/src/test_$i.md"
-    cp /tmp/bench/test.md "/tmp/bench/sapling/benchmark/content/blogs/test_$i.md"
-    cp /tmp/bench/test.md "/tmp/bench/hugo/content/test_$i.md"
+    cp $BASE_DIR/tmp/bench/test.md "$BASE_DIR/tmp/bench/anna/site/content/posts/test_$i.md"
+    cp $BASE_DIR/tmp/bench/test.md "$BASE_DIR/tmp/bench/saaru/docs/src/test_$i.md"
+    cp $BASE_DIR/tmp/bench/test.md "$BASE_DIR/tmp/bench/sapling/benchmark/content/blogs/test_$i.md"
+    cp $BASE_DIR/tmp/bench/test.md "$BASE_DIR/tmp/bench/hugo/content/test_$i.md"
 done
 echo ""
 
@@ -75,10 +93,10 @@ echo "Begin Benchmark"
 echo "running benchmark: $files md files and $warm warmup runs"
 echo ""
 hyperfine -p 'sync' -w $warm \
-  "cd /tmp/bench/hugo && hugo" \
-  "cd /tmp/bench/anna && ./anna" \
-  "cd /tmp/bench/anna && ./anna_greentea" \
-  "cd /tmp/bench/saaru && ./saaru --base-path ./docs" \
-  "cd /tmp/bench/sapling/benchmark && ./../sapling run"
+  "cd $BASE_DIR/tmp/bench/hugo && hugo" \
+  "cd $BASE_DIR/tmp/bench/anna && ./anna" \
+  "cd $BASE_DIR/tmp/bench/anna && ./anna_greentea" \
+  "cd $BASE_DIR/tmp/bench/saaru && ./saaru --base-path ./docs" \
+  "cd $BASE_DIR/tmp/bench/sapling/benchmark && ./../sapling run"
 echo ""
 echo "End Benchmark"
